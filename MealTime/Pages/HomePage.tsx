5f108a92f68a9508cc0495da2c823ef0c9b7be4d
Armah <77860336+armah001@users.noreply.github.com>
2024-06-12 10:56:34 +0000

    Update HomePage.tsx

--- MealTime/Pages/HomePage.tsx
+++ MealTime/Pages/HomePage.tsx
@@ -1,112 +1,103 @@
-import { StyleSheet, Text, View,ImageBackground,TouchableOpacity } from 'react-native';
-import Hero from '../Components/Hero';
-import SelectionTab from '../Components/SelectionTab';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import ConfirmModal from '../Components/ConfirmModal';
 import { useState } from 'react';
 import SuccessCard from '../Components/SucessCard';
+import SelectionTab from '../Components/SelectionTab';
 
 export default function HomePage() {
   const [showSuccessCard, setShowSuccessCard] = useState(false);
   const handleLogout = () => {
     // Implement your logout logic here
-  }; 
+  };
+
   return (
-   <View style={styles.container}>
-<ImageBackground
-      source={require('../assets/burger.avif')} 
-      style={styles.backgroundImage}
-    >
-     
-      <View style={styles.content}>
-        <View style={styles.salutation}>
-        <Text style={styles.text}>Hi, Eric!</Text>
-        <View style={styles.logOutIcon}>
-      <TouchableOpacity onPress={handleLogout}>
-        <MaterialIcons name="logout" size={24} color="white" />
-      </TouchableOpacity>
-    </View>
-        </View>
-  
-        {!showSuccessCard && (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../assets/burger.avif')}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.content}>
+          <View style={styles.salutation}>
+            <Text style={styles.text}>Hi, Eric!</Text>
+            <View style={styles.logOutIcon}>
+              <TouchableOpacity onPress={handleLogout}>
+                <MaterialIcons name="logout" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {!showSuccessCard && (
             <View style={styles.HeroBox}>
               <Text style={styles.HeroText}>Choose Your Meals For The Week</Text>
             </View>
           )}
-      </View>
-      
-    </ImageBackground>
-    {/* <View style={styles.HeroBox}>
-        <Text style={styles.HeroText}>Choose Your Meals For The Week</Text>
-        </View> */}
-        <View style={styles.SelectionTab}>
-        {showSuccessCard ? <SuccessCard /> : <SelectionTab onConfirm={() => setShowSuccessCard(true)} /> }
         </View>
-   </View>
-      
+      </ImageBackground>
+
+      <View style={styles.SelectionTab}>
+        {showSuccessCard ? <SuccessCard /> : <SelectionTab onConfirm={() => setShowSuccessCard(true)} />}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    // alignItems:"center",
-    // justifyContent:"center"
+  container: {
+    flex: 1,
   },
-  SelectionTab:{
-      flex:0.7,
-      width:"100%",
-      height:"100%",
-      marginTop:-190,
-      
+  SelectionTab: {
+    flex: 0.7,
+    width: "100%",
+    height: "100%",
+    marginTop: -100,
   },
   backgroundImage: {
     flex: 0.3,
-    width: "100%", 
-    resizeMode: 'cover', 
-    justifyContent: 'center', 
+    width: "100%",
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   content: {
     flex: 1,
-    flexDirection:"column",
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-    justifyContent:"space-evenly",
-    alignItems:"center",
-    marginTop: -190
-   
+    flexDirection: "column",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginTop: -10
   },
   salutation: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
     flex: 1,
-    flexDirection:"row",
-    width:"100%",
-    justifyContent:"space-between",
-    alignItems:"center"
-   
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
-  text:{
-    fontSize:20,
-    fontWeight:"600",
-    color:"#fff",
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#fff",
     padding: 30,
   },
-  HeroText:{
-    textAlign:"center",
-    color:"#fff",
-    fontSize:35,
-    fontWeight:"600",
-    marginBottom:60
-
+  HeroText: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 35,
+    fontWeight: "600",
+    marginBottom: 85
   },
-  HeroBox:{
-    width:"75%",
+  HeroBox: {
+    width: "75%",
   },
-  logOutIcon:{
+  logOutIcon: {
     justifyContent: 'center',
     alignItems: 'center',
-    width:40,
-    height:40,
-    backgroundColor:"rgba(217,217,217,0.4)",
-    borderRadius:100,
-    marginRight:20,
+    width: 40,
+    height: 40,
+    backgroundColor: "rgba(217,217,217,0.4)",
+    borderRadius: 100,
+    marginRight: 20,
   }
 });
