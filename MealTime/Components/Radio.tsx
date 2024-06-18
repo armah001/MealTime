e163e598c4fe93433e733585fbd08c47923175e6
Martin Luther Famiyeh <martinfamiyeh@gmail.com>
2024-06-18 15:07:39 +0000

    Add meal images

--- MealTime/Components/Radio.tsx
+++ MealTime/Components/Radio.tsx
@@ -1,5 +1,7 @@
-import { View,StyleSheet, Touchable, TouchableOpacity,Text, ImageBackground } from "react-native";
+import { View,StyleSheet, Touchable, TouchableOpacity,Text, ImageBackground ,Dimensions} from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
+
+
 const Radio=({data,checkedValue,onChange,style})=>{
     return (
         <View style={[styles.container,style]}>
@@ -21,7 +23,9 @@ const Radio=({data,checkedValue,onChange,style})=>{
                 </View>
                 <View style={styles.mealNameImage}>
                 <View style={styles.backgroundImage}>
-                    
+                    <ImageBackground source={{uri: meal.image}} style={styles.cardImage} >
+                  
+                    </ImageBackground>
                     </View>
                    
                 <Text style={styles.text}>
@@ -37,7 +41,7 @@ const Radio=({data,checkedValue,onChange,style})=>{
     );
 }
 export default Radio;
-
+const {width, height} = Dimensions.get('screen');
 const styles=StyleSheet.create({
     container:{
         width:"100%",
@@ -53,7 +57,9 @@ const styles=StyleSheet.create({
     text:{
         fontSize:16,
         fontWeight:400,
-        width:"100%"
+        width:"100%",
+        paddingTop:10,
+        textAlign:"left"
     },
     radioView:{
         alignContent:"center",
@@ -67,6 +73,16 @@ const styles=StyleSheet.create({
     backgroundImage:{
         width:40,
         height:40,
-        backgroundColor:"rgba(0,0.5,0.4,0.2)"
-    }
+        //backgroundColor:"rgba(0,0.5,0.4,0.2)"
+    },
+    cardImage: {
+        width: width * 0.12,
+        height: height * 0.05,
+        resizeMode: 'center',
+        borderRadius: 10, 
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:"red"
+    },
 })
